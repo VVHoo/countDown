9c2a7d6c6165a85c22c69d46fc9d4acd86bb9bf9
Hoo <136200456@qq.com>
2018-09-07 16:17:48 +0800

    feat: 增加倒计时格式化

--- countDown.js
+++ countDown.js
@@ -1,31 +1,40 @@
 ;(function($, document, window){
   var CountDown = function (el, options) {
     this.element = el;
-    this.options = {
-      serverBeginTime: options.beginTime,
-      serverEndTime: options.endTime
-    }
+    this.settings = $.extend({format: 'DD:HH:MM'}, options);
     this.init()
   }
   CountDown.prototype = {
     init: function () {
-      this.timeLeft = (this.options.serverEndTime - this.options.serverBeginTime) * 1000;
+      this.timeLeft = this.settings.endTime - this.settings.beginTime;
+      this.match = this.settings.format.split(':')[0].toUpperCase();
       this.timer = null;
       this.interval();
     },
     calculateTime: function () {
       // 开始时间+1秒，重新计算剩余时间
-      this.options.serverBeginTime++;
-      this.timeLeft = (this.options.serverEndTime - this.options.serverBeginTime) * 1000;
+      this.settings.beginTime++;
+      this.timeLeft = this.settings.endTime - this.settings.beginTime;
     },
     interval: function () {
       var self = this;
       // 倒计时计算剩余时间
       this.timer = setInterval(function () {
-        var hour = Math.floor(self.timeLeft / 1000 / 60 / 60 % 24, 10) < 10 ? '0' + Math.floor(self.timeLeft / 1000 / 60 / 60 % 24, 10) : Math.floor(self.timeLeft / 1000 / 60 / 60 % 24, 10);
-        var minutes = Math.floor(self.timeLeft / 1000 / 60 % 60, 10) < 10 ? '0' + Math.floor(self.timeLeft / 1000 / 60 % 60, 10) : Math.floor(self.timeLeft / 1000 / 60 % 60, 10);
-        var seconds = Math.floor(self.timeLeft / 1000 % 60, 10) < 10 ? '0' + Math.floor(self.timeLeft / 1000 % 60, 10) : Math.floor(self.timeLeft / 1000 % 60, 10);
-        self.element.html(hour + ':' + minutes + ':' + seconds);
+        var day = Math.floor(self.timeLeft / 60 / 60 / 24 % 24, 10);
+        var hour = Math.floor(self.timeLeft / 60 / 60 % 24, 10) < 10 ? '0' + Math.floor(self.timeLeft / 60 / 60 % 24, 10) : Math.floor(self.timeLeft / 60 / 60 % 24, 10);
+        var minutes = Math.floor(self.timeLeft / 60 % 60, 10) < 10 ? '0' + Math.floor(self.timeLeft / 60 % 60, 10) : Math.floor(self.timeLeft / 60 % 60, 10);
+        var seconds = Math.floor(self.timeLeft % 60, 10) < 10 ? '0' + Math.floor(self.timeLeft % 60, 10) : Math.floor(self.timeLeft % 60, 10);
+        switch (self.match) {
+          case 'DD':
+            self.element.html('剩余' + day + '天' + hour + '小时' + minutes + '分' + seconds + '秒');
+            break;
+          case 'HH':
+            self.element.html('剩余' + hour + '小时' + minutes + '分' + seconds + '秒');
+            break;
+          case 'MM':
+            self.element.html('剩余' + minutes + '分' + seconds + '秒');
+            break;
+        }
         self.calculateTime();
       }, 1000)
     }
